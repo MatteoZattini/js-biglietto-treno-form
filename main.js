c558d01b6dff5a2e56111660fd1aa26c1f5215ca
MatteoZattini <matteozattini@hotmail.com>
2024-04-18 17:08:59 +0200

    added if

--- main.js
+++ main.js
@@ -1,19 +1,23 @@
 const tariffa = (0.21)
-const Prezzo = ( userKm.value * tariffa)
 const userName = document.querySelector('#UserName')
 const userAge = document.querySelector('#UserAge')
 const userKm = document.querySelector('#UserKm')
 const myButton = document.querySelector('.btn')
-const ScontoJunior = (Prezzo * 20 / 100)
-const ScontoSenior = (Prezzo * 40 / 100)
 
 myButton.addEventListener('click', function() {
     console.log(userAge.value);
     console.log(parseInt(userAge.value));
     console.log(userKm.value);
     console.log(parseInt(userKm.value));
+    const Prezzo = (userKm.value * tariffa)
+    const ScontoJunior = (Prezzo * 20 / 100)
+    const ScontoSenior = (Prezzo * 40 / 100)
     if(userAge.value < 18) {
         console.log(Prezzo - ScontoJunior)
+    }  else if (userAge.value > 65) {
+        console.log(Prezzo - ScontoSenior)
+    } else if (userAge.value >= 18 && userAge.value < 65) {
+        console.log(Prezzo)
     }
-})
+}) 
 
