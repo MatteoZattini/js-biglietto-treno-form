758c4ecca3379ea8ee05da2c2283207eeffed28f
MatteoZattini <matteozattini@hotmail.com>
2024-04-18 18:20:14 +0200

    added full exercise

--- main.js
+++ main.js
@@ -5,6 +5,9 @@ const userKm = document.querySelector('#UserKm')
 const myButton = document.querySelector('.btn')
 
 myButton.addEventListener('click', function() {
+    console.log(userName.value);
+    let Nome = (userName.value)
+    document.getElementById('User').innerHTML = Nome
     console.log(userAge.value);
     console.log(parseInt(userAge.value));
     console.log(userKm.value);
@@ -12,12 +15,28 @@ myButton.addEventListener('click', function() {
     const Prezzo = (userKm.value * tariffa)
     const ScontoJunior = (Prezzo * 20 / 100)
     const ScontoSenior = (Prezzo * 40 / 100)
+    let randomN = Math.floor(Math.random() * 10) + 1;
+    document.getElementById('SlotN').innerHTML = randomN
     if(userAge.value < 18) {
         console.log(Prezzo - ScontoJunior)
+        let prezzosconto = (Prezzo - ScontoJunior)
+        let n = prezzosconto.toFixed(2)
+        console.log(n)
+        document.getElementById('Costo').innerHTML = n + "€"
+        document.getElementById('TicketType').innerHTML = "Junior"
     }  else if (userAge.value > 65) {
         console.log(Prezzo - ScontoSenior)
+        let prezzosconto = (Prezzo - ScontoSenior)
+        let n = prezzosconto.toFixed(2)
+        console.log(n)
+        document.getElementById('Costo').innerHTML = n + "€"
+        document.getElementById('TicketType').innerHTML = "Senior"
     } else if (userAge.value >= 18 && userAge.value < 65) {
         console.log(Prezzo)
+        let n = Prezzo.toFixed(2)
+        console.log(n)
+        document.getElementById('Costo').innerHTML = n + "€"
+        document.getElementById('TicketType').innerHTML = "Standard"
     }
 }) 
 
